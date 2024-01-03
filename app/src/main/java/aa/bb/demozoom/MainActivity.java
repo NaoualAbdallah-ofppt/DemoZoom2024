22387a39c54b2fa32ce808ad8d18bec650046cef
NaoualAbdallah-ofppt <NaoualAbdallah@hotmail.com>
2024-01-03 09:55:32 +0100

    second commit

--- app/src/main/java/aa/bb/demozoom/MainActivity.java
+++ app/src/main/java/aa/bb/demozoom/MainActivity.java
@@ -32,17 +32,21 @@ img.setOnTouchListener(new View.OnTouchListener() {
         switch(motionEvent.getAction() & MotionEvent.ACTION_MASK){
 
             case MotionEvent.ACTION_POINTER_DOWN:
+                if (motionEvent.getPointerCount()>1)
+                {
                 distanceX = motionEvent.getX(0) - motionEvent.getX(1);
                 distanceY = motionEvent.getY(0) - motionEvent.getY(1);
                 distanceDepart = Math.sqrt(
-                        distanceX * distanceX + distanceY * distanceY);
+                        distanceX * distanceX + distanceY * distanceY);}
                 break;
             case MotionEvent.ACTION_MOVE:
-                distanceX = motionEvent.getX(0) - motionEvent.getX(1);
-                distanceY = motionEvent.getY(0) - motionEvent.getY(1);
-                distanceFin = Math.sqrt(
-                        distanceX * distanceX + distanceY * distanceY);
-                redimensionner();
+                if (motionEvent.getPointerCount()>1) {
+                    distanceX = motionEvent.getX(0) - motionEvent.getX(1);
+                    distanceY = motionEvent.getY(0) - motionEvent.getY(1);
+                    distanceFin = Math.sqrt(
+                            distanceX * distanceX + distanceY * distanceY);
+                    redimensionner();
+                }
                 break;
         }
 
